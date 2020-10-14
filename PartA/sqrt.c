15772fb0cb296c23492df58744e711ede7ff0c31
op-guy <chughsaatvik@gmail.com>
2020-10-14 19:30:29 +0530

    Added negative input support

--- PartA/sqrt.c
+++ PartA/sqrt.c
@@ -5,7 +5,9 @@ positive number. */
 #include <stdlib.h>
 #include <math.h>
 
-
+int abs(int x) {
+	return (x < 0)? (-1)*x:x;
+}
 // Main function
 int main(int argc, char* argv[]) {
 	
@@ -15,13 +17,9 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
-
-	if(input < 0) {
-		printf("Input cannot be negative\n");
-		exit(-1);
-	}
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
-	printf("Exiting the program\nExiting...\n");
+	printf("Sqrt of %d is %f",input,sqrt(abs(input)));
+	if(input < 0) printf("i");
+	printf("\nExiting the program\nExiting...\n");
 	return(0);
 
 } // end main
